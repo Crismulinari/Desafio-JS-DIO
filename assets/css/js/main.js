77cd340ff43a7d14e2a9b9dabc80d2abebe80229
Cristiano Agnaldo Mulinari <62948211+Crismulinari@users.noreply.github.com>
2022-12-01 14:13:31 -0300

    segunda tentativa

--- assets/css/js/main.js
+++ assets/css/js/main.js
@@ -1,29 +1,38 @@
-function convertPersonToLi(person){
-    return     `
-        <li class="person">
-            <span class="number">#000${person.id}</span>
-            <span class="name">${person.name}</span>
-            <div class="detail">
-                <ol class="referens">
-                    <li class="referen">height: ${person.height}</li>
-                    <li class="referen">mass: ${person.mass}</li>
-                    <li class="referen">gender: ${person.gender}</li>
-                    <li class="referen">species: ${person.species}</li>i
-                </ol>
-                <img src="${person.image}"
-                     alt="${person.name}">
-            </div>
-        </li>
-    `
-}
 const personList = document.getElementById("personsList")
+const loadMoreButton = document.getElementById('loadMoreButton')
+const limit = 5;
+let offset = 0;
 
-starWarApi.getPersons ().then((persons = []) => {  
-    const newHtml = persons.map(convertPersonToLi).join('') 
-    personList.innerHTML = newHtml   
+function loadPersonItens(offset, limit){
+    starWarApi.getPersons (offset, limit).then((persons = []) => {  
+        const newHtml = persons.map((person) =>  `
+            <li class="person">            
+                <span class="name">${person.name}</span>
+                <span class="number">homeworld: ${person.homeworld}</span>
+                <div class="detail">
+                    <ol class="referens">
+                        <li class="referen">height: ${person.height}</li>
+                        <li class="referen">mass: ${person.mass}</li>
+                        <li class="referen">gender: ${person.gender}</li>
+                        <li class="referen">species: ${person.species}</li>
+                    </ol>
+                    <img src="${person.image}"
+                        alt="${person.name}">
+                </div>
+            </li>
+        `).join('') 
+        personList.innerHTML += newHtml   
     })
+}
+
+loadPersonItens(offset, limit)
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    loadPersonItens(offset, limit)
+})
     
     
-    .catch((error) => console.error(error))   
+    
     
    
--- assets/css/js/starwar-api.js
+++ assets/css/js/starwar-api.js
@@ -1,6 +1,6 @@
 const starWarApi = {}
 
-starWarApi.getPersons = (offset = 0, limit = 10) => {    
+starWarApi.getPersons = (offset = 0, limit = 5) => {    
     const url = `https://akabab.github.io/starwars-api/api/all.json?offset=${offset}&limit=${limit}`
     return fetch(url)
     .then((response) => response.json())
